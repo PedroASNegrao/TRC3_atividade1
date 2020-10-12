cfee1bb9ceb33675a603c745db1418b44fe280cd
Pedo Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2020-10-12 09:52:32 -0300

    routes update

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import DiciplinaController from "./app/controllers/DiciplinaController";
 
 const routes = new Router();
 
-routes.get("/teste", DiciplinaController.teste);
+routes.get("/", DiciplinaController.teste);
 routes.get("/disciplinas", DiciplinaController.index);
 routes.post("/disciplinas", DiciplinaController.store);
 routes.put("/disciplinas/:id", DiciplinaController.update);
